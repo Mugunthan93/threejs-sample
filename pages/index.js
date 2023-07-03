811cf67276ac4bfdb4fc917e133529e2ca08b00b
Mugunthan93 <kskarthick93@gmail.com>
2023-07-03 14:44:48 +0530

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,44 @@
-import styles from '../styles/Home.module.css';
 import * as THREE from 'three';
+import { useEffect } from 'react';
 
 export default function Home() {
-  // const scene = new THREE.Scene();
-  // const camera = new THREE.PerspectiveCamera(
-  //   75,
-  //   window.innerWidth / window.innerHeight,
-  //   0.1,
-  //   1000
-  // );
-  // const renderer = new THREE.WebGLRenderer();
-  // renderer.setSize(window.innerWidth, window.innerHeight);
-  // document.body.appendChild(renderer.domElement);
-  return <div className={styles.container}></div>;
+  const createScene = () => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    function animate() {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    }
+
+    animate();
+  };
+
+  useEffect(() => {
+    return () => {
+      createScene();
+    };
+  }, [createScene]);
+
+  return <div></div>;
 }
