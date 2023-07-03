b42f61df6ff901a700a3b6e82427ad124431462c
Mugunthan93 <kskarthick93@gmail.com>
2023-07-03 12:23:18 +0530

    3 files modified

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
-import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import * as THREE from 'three';
 
 export default function Home() {
+  // const scene = new THREE.Scene();
+  // const camera = new THREE.PerspectiveCamera(
+  //   75,
+  //   window.innerWidth / window.innerHeight,
+  //   0.1,
+  //   1000
+  // );
+  // const renderer = new THREE.WebGLRenderer();
+  // renderer.setSize(window.innerWidth, window.innerHeight);
+  // document.body.appendChild(renderer.domElement);
   return <div className={styles.container}></div>;
 }
